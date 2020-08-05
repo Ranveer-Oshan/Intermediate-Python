d6a10cca5d94440976d1c9aaf89e5808e859ab8a
Ranveer-Oshan <54892918+Ranveer-Oshan@users.noreply.github.com>
2020-08-05 23:47:36 +0530

    Create dictionaries-pandas?ex=6.py

---
+++ Dictionaries/dictionaries-pandas?ex=6.py
@@ -0,0 +1,14 @@
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo' }
+
+# Add italy to europe
+europe['italy'] = 'rome'
+
+# Print out italy in europe
+print('italy' in europe)
+
+# Add poland to europe
+europe['poland'] = 'warsaw'
+
+# Print europe
+print(europe)
