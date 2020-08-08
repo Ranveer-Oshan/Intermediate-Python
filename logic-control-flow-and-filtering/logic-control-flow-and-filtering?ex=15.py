88510b8f05a7729d9bc07b0ce7de419bc2240de4
Ranveer-Oshan <54892918+Ranveer-Oshan@users.noreply.github.com>
2020-08-08 13:07:05 +0530

    Create logic-control-flow-and-filtering?ex=15.py

---
+++ logic-control-flow-and-filtering/logic-control-flow-and-filtering?ex=15.py
@@ -0,0 +1,9 @@
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Extract drives_right column as Series: dr
+dr = cars["drives_right"]
+
+# Use dr to subset cars: sel
+sel = cars[dr]
