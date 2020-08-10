bcc43d781400971c1066ffffe6ed70b0f891371c
Ranveer-Oshan <54892918+Ranveer-Oshan@users.noreply.github.com>
2020-08-10 11:05:34 +0530

    Create loops?ex=16.py

---
+++ loops/loops?ex=16.py
@@ -0,0 +1,11 @@
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Code for loop that adds COUNTRY column
+for lab, row in cars.iterrows() :
+    cars.loc[lab , "COUNTRY"] = row["country"].upper()
+
+
+# Print cars
+print(cars)
