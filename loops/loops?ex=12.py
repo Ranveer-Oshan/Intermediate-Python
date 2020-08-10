ed9c34bcd7ddbcbdce15ef20e7a7fc56a24dcfaa
Ranveer-Oshan <54892918+Ranveer-Oshan@users.noreply.github.com>
2020-08-10 10:30:36 +0530

    Create loops?ex=12.py

---
+++ loops/loops?ex=12.py
@@ -0,0 +1,11 @@
+# Import numpy as np
+import numpy as np
+
+# For loop over np_height
+for height in np_height :
+    print(str(height) + " inches")
+
+
+# For loop over np_baseball
+for height in np.nditer(np_baseball) :
+    print(height)
