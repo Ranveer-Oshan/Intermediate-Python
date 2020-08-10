54910fd030657b3e01136f99ad50d6b2244447eb
Ranveer-Oshan <54892918+Ranveer-Oshan@users.noreply.github.com>
2020-08-10 11:26:51 +0530

    Create case-study-hacker-statistics?ex=3.py

---
+++ case-study-hacker-statistics/case-study-hacker-statistics?ex=3.py
@@ -0,0 +1,9 @@
+# Import numpy and set seed
+import numpy as np
+np.random.seed(123)
+
+# Use randint() to simulate a dice
+print(np.random.randint(1,7))
+
+# Use randint() again
+print(np.random.randint(1,7))
