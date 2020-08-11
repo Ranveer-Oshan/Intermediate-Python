e7a3961793b0c4245c60299ddae2508e27ca8518
Ranveer Oshan <ranveermedran@gmail.com>
2020-08-11 21:42:48 +0530

    Create case-study-hacker-statistics?ex=7.py

---
+++ case-study-hacker-statistics/case-study-hacker-statistics?ex=7.py
@@ -0,0 +1,20 @@
+# Numpy is imported, seed is set
+
+# Initialize random_walk
+random_walk = [0]
+
+for x in range(100) :
+    step = random_walk[-1]
+    dice = np.random.randint(1,7)
+
+    if dice <= 2:
+        # Replace below: use max to make sure step can't go below 0
+        step = max(0, step - 1)
+    elif dice <= 5:
+        step = step + 1
+    else:
+        step = step + np.random.randint(1,7)
+
+    random_walk.append(step)
+
+print(random_walk)
