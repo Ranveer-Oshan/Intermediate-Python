51d3dcf807519414b242f53cb050d5ddcc563c38
Ranveer Oshan <ranveermedran@gmail.com>
2020-08-11 21:47:29 +0530

    Create case-study-hacker-statistics?ex=8.py

---
+++ case-study-hacker-statistics/case-study-hacker-statistics?ex=8.py
@@ -0,0 +1,26 @@
+# Numpy is imported, seed is set
+
+# Initialization
+random_walk = [0]
+
+for x in range(100) :
+    step = random_walk[-1]
+    dice = np.random.randint(1,7)
+
+    if dice <= 2:
+        step = max(0, step - 1)
+    elif dice <= 5:
+        step = step + 1
+    else:
+        step = step + np.random.randint(1,7)
+
+    random_walk.append(step)
+
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# Plot random_walk
+plt.plot(random_walk)
+
+# Show the plot
+plt.show()
