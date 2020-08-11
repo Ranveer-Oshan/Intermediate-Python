ef4df1538f582f6af6f7a31b4fc5569a294becb5
Ranveer Oshan <ranveermedran@gmail.com>
2020-08-11 22:02:00 +0530

    Create case-study-hacker-statistics?ex=11.py

---
+++ case-study-hacker-statistics/case-study-hacker-statistics?ex=11.py
@@ -0,0 +1,33 @@
+# numpy and matplotlib imported, seed set.
+
+# initialize and populate all_walks
+all_walks = []
+for i in range(10) :
+    random_walk = [0]
+    for x in range(100) :
+        step = random_walk[-1]
+        dice = np.random.randint(1,7)
+        if dice <= 2:
+            step = max(0, step - 1)
+        elif dice <= 5:
+            step = step + 1
+        else:
+            step = step + np.random.randint(1,7)
+        random_walk.append(step)
+    all_walks.append(random_walk)
+
+# Convert all_walks to Numpy array: np_aw
+np_aw = np.array(all_walks)
+
+# Plot np_aw and show
+plt.plot(np_aw)
+plt.show()
+# Clear the figure
+plt.clf()
+
+# Transpose np_aw: np_aw_t
+np_aw_t = np.transpose(np_aw)
+
+# Plot np_aw_t and show
+plt.plot(np_aw_t)
+plt.show()
