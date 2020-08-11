068721cd160800425afea2cd360540dfaac399de
Ranveer Oshan <ranveermedran@gmail.com>
2020-08-11 22:06:09 +0530

    Create case-study-hacker-statistics?ex=12.py

---
+++ case-study-hacker-statistics/case-study-hacker-statistics?ex=12.py
@@ -0,0 +1,27 @@
+# numpy and matplotlib imported, seed set
+
+# Simulate random walk 250 times
+all_walks = []
+for i in range(250) :
+    random_walk = [0]
+    for x in range(100) :
+        step = random_walk[-1]
+        dice = np.random.randint(1,7)
+        if dice <= 2:
+            step = max(0, step - 1)
+        elif dice <= 5:
+            step = step + 1
+        else:
+            step = step + np.random.randint(1,7)
+
+        # Implement clumsiness
+        if(np.random.rand() <= 0.001) :
+            step = 0
+
+        random_walk.append(step)
+    all_walks.append(random_walk)
+
+# Create and plot np_aw_t
+np_aw_t = np.transpose(np.array(all_walks))
+plt.plot(np_aw_t)
+plt.show()
